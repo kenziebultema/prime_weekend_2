8344743aaf3e42d2012231993beae4a9d8e84de0
Kenzie Bultema <krbultema@gmail.com>
2015-12-19 20:00:47 -0600

    first attempt, added charlie to theta list

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,14 +1,89 @@
-$(document).ready(function(){
-    getData();
-});
+var personTracker = 0;
+var theta;
 
-function getData(){
+$(document).ready(function(){
+
     $.ajax({
         type: "GET",
         url:"/data",
         success: function(data){
-            console.log(data);
+            // store into variable for readability
+            theta = data.people;
+            console.log(theta);
+
+            createNav(data);
+            putStuffIn(data);
+            fades();
+
+            $('#buttonContainer').on('click', '.next',  nextPerson);
+            $('#buttonContainer').on('click', '.prev',  prevPerson);
+            clearContainer();
+
         }
 
     });
+
+});
+
+// create prev and next buttons
+function createNav(data) {
+    $('#buttonContainer').append('<button class="prev">Prev</button>');
+
+    $('#buttonContainer').append('<button class="next">Next</button>');
+}
+
+//go to next person from button click
+function nextPerson() {
+    personTracker++;
+    if(personTracker >= theta.length){
+        personTracker = 0;
+    }
+    fades();
+    clearContainer();
+
+}
+
+// go to previous person
+function prevPerson() {
+    personTracker--;
+    if(personTracker < 0){
+        personTracker = theta.length -1;
+    }
+
+    fades();
+    clearContainer();
+}
+
+// put class data into dom
+function putStuffIn(data){
+
+    for(var i = 0; i < theta.length; i++) {
+        $('#peopleContainer').append('<div class="person-container"' + i + '">');
+        var person = theta[i].name;
+        var location = theta[i].location;
+        var animal = theta[i].animal;
+        $('.person-container' + i).append('<p class="person">' + person + '</p><p class="location">' + location + '</p><p class="animal">' + animal + '</p>');
+
+    }
+}
+
+// fade in and out
+function fades() {
+    for(var i = 0; i < theta.length; i++) {
+        $('.person-container' + i).fadeOut();
+
+        if(i == personTracker) {
+            $('.person-container' + i).fadeIn();
+        }
+    }
+}
+
+// attempt to clear dom for next person
+function clearContainer() {
+    //$('#peopleContainer').text(theta[personTracker]);
+    for(var i = 0; i < theta.length; i++){
+        if(i != personTracker){
+            $('.person-container' + i).hide();
+        }
+    }
 }
